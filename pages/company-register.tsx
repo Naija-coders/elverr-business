b31b394c2dafe041e4aea3174d69ee387be32609
Oghenemano <oghenemano@MacBook-Pro.local>
2022-09-26 09:36:43 +0300

    removing unused page

--- pages/company-register.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import CompanyRegister from "../components/Authentication/SignUp/CompanyRegister";
-interface IAppProps {}
-
-const CompanyRegistration: React.FunctionComponent<IAppProps> = (props) => {
-  return <CompanyRegister />;
-};
-
-export default CompanyRegistration;

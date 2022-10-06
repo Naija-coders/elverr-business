08a30db53d31ee879312460a0b56d56028b7cc97
oghenemano omogha <manoomogha@yahoo.com>
2022-10-06 16:46:50 +0300

    adding middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const {cookies} = request
+  const jwt = cookies?.auth_token;
+  if (request.nextUrl.pathname.startsWith('/')) {
+try {
+  if(jwt=== undefined){
+    return NextResponse.rewrite(new URL('/', request.url))
+  }
+  else{
+    return NextResponse.rewrite(new URL('/', request.url))
+  }
+
+} catch (error) {
+  
+}
+  }
+
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+  }
+}

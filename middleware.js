05a2808b3072f4b2963c29b1c4d5bc000e70ad45
oghenemano omogha <manoomogha@yahoo.com>
2022-10-08 17:34:10 +0300

    url

--- middleware.js
+++ middleware.js
@@ -5,11 +5,14 @@ export default function middleware(req) {
   let url = req.url;
 
   if (!verify && url.includes("/mainpage")) {
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_URL);
+    return NextResponse.redirect("https://business.elverr.com/");
   }
 
-  if (verify && url === process.env.NEXT_PUBLIC_URL) {
-    return NextResponse.rewrite(process.env.NEXT_PUBLIC_URL + "mainpage");
+  if (verify && url === "https://business.elverr.com/") {
+    return NextResponse.rewrite("https://business.elverr.com/mainpage");
+  }
+  if (verify && url === "http://localhost:3000/") {
+    return NextResponse.rewrite("http://localhost:3000/mainpage");
   }
 }
 /* return NextResponse.rewrite(new URL('/dashboard/user', request.url)) */

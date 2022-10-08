24f2f94101be68585b9e740e4253b489c56ef08b
oghenemano omogha <manoomogha@yahoo.com>
2022-10-08 21:24:21 +0300

    s

--- middleware.js
+++ middleware.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { NextResponse } from "next/server";
-
-export default function middleware(req) {
-  let verify = req.cookies.get("auth_token");
-  let url = req.url;
-
-  if (!verify && url.includes("/mainpage")) {
-    return NextResponse.redirect("https://business.elverr.com/");
-  }
-
-  if (verify && url === "https://business.elverr.com/") {
-    return NextResponse.rewrite("https://business.elverr.com/mainpage");
-  }
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.rewrite("http://localhost:3000/mainpage");
-  }
-}
-/* return NextResponse.rewrite(new URL('/dashboard/user', request.url)) */
-=======
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req) {
@@ -41,4 +21,3 @@ export async function middleware(req) {
     }
   }
 }
->>>>>>> feature/front-end

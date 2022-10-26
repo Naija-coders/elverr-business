e4ae5414e19ac375496fc39de2f99b2ed78c908a
oghenemano omogha <manoomogha@yahoo.com>
2022-10-26 14:57:40 +0300

    adding skeleton

--- components/CustomCard/ServiceGridCard.tsx
+++ components/CustomCard/ServiceGridCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CustomDiv, CustomContainer } from "./styles";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { Avatar, Button, IconButton, Typography } from "@mui/material";
+import Skeleton from "@mui/material/Skeleton";
 type Props = {
   data: any;
 };
@@ -9,41 +10,10 @@ type Props = {
 export default function ServiceGridCard({ data }: Props) {
   return (
     <div>
-      <CustomDiv>
-        {data?.map((item: any) => (
-          <CustomContainer key={Math.random()}>
-            <img
-              src={item.image_url}
-              style={{
-                height: "173.px",
-                borderRadius: "16.7039px 16.7039px 0px 0px",
-                width: "100%",
-              }}
-            />
-            <div
-              style={{
-                marginTop: "-45px",
-                display: "flex",
-                justifyContent: "end",
-                marginRight: "10px",
-              }}
-            >
-              <div
-                style={{
-                  width: "32px",
-                  height: "32px",
-                  background: "#F7F7F7",
-                  borderRadius: "50px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <FavoriteBorderIcon
-                  sx={{ fontSize: "20px", color: "#34A422" }}
-                />
-              </div>
-            </div>
+      {data === undefined ? (
+        <CustomDiv>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
             <div
               style={{
                 display: "flex",
@@ -56,6 +26,7 @@ export default function ServiceGridCard({ data }: Props) {
                 width: "100%",
               }}
             >
+              {" "}
               <div
                 style={{
                   display: "flex",
@@ -63,48 +34,443 @@ export default function ServiceGridCard({ data }: Props) {
                   gap: "1rem",
                   alignItems: "center",
                   justifyContent: "space-between",
+                  width: "100%",
                 }}
               >
-                <Avatar
-                  /* src={item?.profile_photo_path} */ src={item.image_url}
-                />
-                <div>
-                  <Typography
-                    sx={{
-                      fontWeight: 600,
-                      fontSize: "0.9rem",
-                      fontFamily: "DM Sans",
-                      color: "#303030",
-                    }}
-                  >
-                    {item?.name}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      fontWeight: 300,
-                      fontSize: "0.8rem",
-                      color: " #747582",
-                      fontFamily: "DM Sans",
-                    }}
-                  >
-                    {item?.type}
-                  </Typography>
-                </div>
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
               </div>
-              <Typography
-                sx={{
-                  fontWeight: 700,
-                  fontSize: "0.95rem",
-                  color: "#34A422",
-                  fontFamily: "DM Sans",
-                }}
-              >
-                $ {item?.price}
-              </Typography>
             </div>
           </CustomContainer>
-        ))}
-      </CustomDiv>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>{" "}
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>{" "}
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+          <CustomContainer>
+            <Skeleton variant="rectangular" width={"100%"} height={"173px"} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                paddingLeft: "10px",
+                paddingRight: "1rem",
+                paddingTop: "0.5rem",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: "1rem",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="text" width={"70%"} />
+              </div>
+            </div>
+          </CustomContainer>
+        </CustomDiv>
+      ) : (
+        <CustomDiv>
+          {data?.map((item: any) => (
+            <CustomContainer key={Math.random()}>
+              <img
+                src={item.image_url}
+                style={{
+                  height: "173px",
+                  borderRadius: "16.7039px 16.7039px 0px 0px",
+                  width: "100%",
+                }}
+              />
+              <div
+                style={{
+                  marginTop: "-45px",
+                  display: "flex",
+                  justifyContent: "end",
+                  marginRight: "10px",
+                }}
+              >
+                <div
+                  style={{
+                    width: "32px",
+                    height: "32px",
+                    background: "#F7F7F7",
+                    borderRadius: "50px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <FavoriteBorderIcon
+                    sx={{ fontSize: "20px", color: "#34A422" }}
+                  />
+                </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  paddingLeft: "10px",
+                  paddingRight: "1rem",
+                  paddingTop: "0.5rem",
+                  justifyContent: "space-between",
+                  marginTop: "10px",
+                  width: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "1rem",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Avatar
+                    /* src={item?.profile_photo_path} */ src={item.image_url}
+                  />
+                  <div>
+                    <Typography
+                      sx={{
+                        fontWeight: 600,
+                        fontSize: "0.9rem",
+                        fontFamily: "DM Sans",
+                        color: "#303030",
+                      }}
+                    >
+                      {item?.name}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontWeight: 300,
+                        fontSize: "0.8rem",
+                        color: " #747582",
+                        fontFamily: "DM Sans",
+                      }}
+                    >
+                      {item?.type}
+                    </Typography>
+                  </div>
+                </div>
+                <Typography
+                  sx={{
+                    fontWeight: 700,
+                    fontSize: "0.95rem",
+                    color: "#34A422",
+                    fontFamily: "DM Sans",
+                  }}
+                >
+                  $ {item?.price}
+                </Typography>
+              </div>
+            </CustomContainer>
+          ))}
+        </CustomDiv>
+      )}
     </div>
   );
 }

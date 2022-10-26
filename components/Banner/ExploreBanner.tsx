f99848498479030cd76820250f266542c6763984
oghenemano omogha <manoomogha@yahoo.com>
2022-10-26 12:10:03 +0300

    adding world map

--- components/Banner/ExploreBanner.tsx
+++ components/Banner/ExploreBanner.tsx
@@ -6,6 +6,7 @@ import { StyledBox, StyleContainer } from "../NotLoggedIn/style";
 import { Text, MainDiv1, MainDivRight } from "./styles";
 import BannerFilter from "./BannerFilter";
 import { CustomTypography1, CustomTypography2 } from "./styles";
+import { Button } from "@mui/material";
 export default function DiscoverBanner({}: Props) {
   return (
     <StyledBox>
@@ -13,13 +14,108 @@ export default function DiscoverBanner({}: Props) {
         <div
           style={{
             height: "404px",
-            marginTop: "-32px",
+
             position: "relative",
-            background:
-              "linear-gradient(90deg, rgba(54, 157, 41, 0.79) 0%, #369D29 100%)",
+
             borderRadius: " 0px 0px 20px 20px",
+            marginTop: "30px",
           }}
-        ></div>
+        >
+          <div
+            style={{
+              width: "150.53px",
+              position: "absolute",
+              left: "10px",
+            }}
+          >
+            <CustomTypography1
+              sx={{ color: "black", fontWeight: 900, fontFamily: "DM Sans" }}
+            >
+              Build
+            </CustomTypography1>
+            <div style={{ display: "flex", gap: "5px", marginTop: "-15px" }}>
+              <CustomTypography1
+                sx={{ color: "black", fontWeight: 900, fontFamily: "DM Sans" }}
+              >
+                And
+              </CustomTypography1>
+              <CustomTypography1
+                sx={{ color: "black", fontWeight: 900, fontFamily: "DM Sans" }}
+              >
+                Grow
+              </CustomTypography1>
+            </div>
+            <div style={{ display: "flex", gap: "5px", marginTop: "-15px" }}>
+              <CustomTypography1
+                sx={{ color: "black", fontWeight: 900, fontFamily: "DM Sans" }}
+              >
+                With
+              </CustomTypography1>
+              <CustomTypography1
+                sx={{
+                  color: "#34A422;",
+                  fontWeight: 900,
+                  fontFamily: "DM Sans",
+                }}
+              >
+                Elverr
+              </CustomTypography1>
+            </div>
+            <CustomTypography1
+              sx={{
+                color: "#34A422;",
+                fontWeight: 900,
+                fontFamily: "DM Sans",
+                marginTop: "-15px",
+              }}
+            >
+              Business.
+            </CustomTypography1>
+            <CustomTypography2
+              sx={{
+                color: " #4E4D4D",
+                width: "280px",
+                fontSize: "0.85rem",
+                fontFamily: "DM Sans",
+                marginTop: "0.5rem",
+                fontWeight: 300,
+              }}
+            >
+              We believe that outsourcing is a lifeline for businesses that are
+              looking to take advantage of the flexibility that comes with
+              working with an outside team.
+            </CustomTypography2>
+            <Button
+              variant="contained"
+              disableElevation
+              sx={{
+                "textTransform": "none",
+                "background": "#34A422",
+                "marginTop": "1.2rem",
+                "fontSize": "0.8rem",
+                "fontFamily": "DM Sans",
+                "&:hover": { background: "#34A422" },
+              }}
+            >
+              Get Better Rate
+            </Button>
+          </div>
+          <div
+            style={{
+              background: "rgba(52, 164, 34, 0.1)",
+              filter: " blur(36.4456px)",
+              position: "absolute",
+              left: "40px",
+              width: "280px",
+              height: "200px",
+              marginTop: "100px",
+              borderRadius: "10px",
+            }}
+          ></div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img src="https://res.cloudinary.com/dxsmdvaqq/image/upload/v1666771765/worldmap.png" />
+          </div>
+        </div>
       </StyleContainer>
     </StyledBox>
   );
--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import DiscoverBanner from "../../components/Banner/DiscoverBanner";
+import ExploreBanner from "../../components/Banner/ExploreBanner";
 import Filter from "../../components/Filters/ExploreFilter";
 import ExploreServices from "../../components/PaginatedServices/ExploreServices";
 import FooterLoggedIn from "../../components/LoggedIn/FooterLoggedIn";
@@ -11,7 +11,7 @@ export default function index({}: Props) {
   return (
     <div>
       <Navbar nosubbar={false} />
-      <DiscoverBanner />
+      <ExploreBanner />
 
       <ExploreServices />
       <FooterLoggedIn />
